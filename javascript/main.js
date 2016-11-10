d4408f46f2750742701fd59ef86fe9842463dbf7
Sammia Karen Zubizarreta Arimuya <Sammia Karen Zubizarreta Arimuya>
2016-11-10 00:08:32 -0500

    validando numero

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 	$(".loading").fadeOut(4000);
 });
+/*
+$(document).ready(function(){
+	$("#envioNumero").click(soloNumeros);
+
+
+});
+
+function ellipsis_box(elemento, max_chars){
+	limite_text = $(elemento).text();
+	if (limite_text.length > max_chars){
+		limite = limite_text.substr(0, max_chars)+" ...";
+		$(elemento).text(limite);
+	}
+}
+	$(function(){
+		ellipsis_box(".limitado", 100);
+	});*/

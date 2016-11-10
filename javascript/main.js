80a91c1b2dff0567c42bc0d4b459c20393db935a
sammia28 <sammia.zubizarreta@gmail.com>
2016-11-10 11:59:04 -0500

    Validar número

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,9 @@
 $(document).ready(function(){
 	$(".loading").fadeOut(4000);
 });
-/*
-$(document).ready(function(){
-	$("#envioNumero").click(soloNumeros);
 
-
+$(document).ready(function (){
+	$('#first-number').keyup(function (){
+		this.value = (this.value + '').replace(/[^0-9]/g, '');
+	});
 });
-
-function ellipsis_box(elemento, max_chars){
-	limite_text = $(elemento).text();
-	if (limite_text.length > max_chars){
-		limite = limite_text.substr(0, max_chars)+" ...";
-		$(elemento).text(limite);
-	}
-}
-	$(function(){
-		ellipsis_box(".limitado", 100);
-	});*/

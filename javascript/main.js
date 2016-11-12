6710ec13bec935b3bea4b5b8fc10554b9db62242
Sammia Karen Zubizarreta Arimuya <Sammia Karen Zubizarreta Arimuya>
2016-11-12 13:01:31 -0500

    almacenar numeros

--- javascript/main.js
+++ javascript/main.js
@@ -2,33 +2,73 @@ $(document).ready(function(){
 	$(".loading").fadeOut(4000);
 });
 
+
 $(document).ready(function (){
 	$("#first-number").keyup(function (){
 		this.value = (this.value + "").replace(/[^0-9]/g, '');
+		window.localStorage.setItem("numCelular",obtNum);
 	});
+
+
 	$("#envioNumero").click(function () {  
 		if($("#first-number").val().length == 9) { 
+
 			window.localStorage.setItem("numeroRandom", parseInt(Math.random() * 10).toString() + parseInt(Math.random().toString() * 10) + parseInt(Math.random() * 10).toString() + parseInt(Math.random() * 10).toString());
 			alert(window.localStorage.getItem("numeroRandom"));
 			return true;  
-		}  
-		return false;  
-	}); 	
+		}else{
+			return false;
+		}
+
+		$("#cel").text("lalal");
+
+	}); 
+
+
+	$("#envioNumero").click(function(){
+		
+		$("#cel").text(localStorage.getItem("numCelular"));
+		var obtNum = $("#numCelular").val();
+
+
+	});
+ 
+	$("#celu").text(localStorage.getItem("celular")); 
+var obtNum = $("#numeros").val();
+		localStorage.setItem("celular", obtNum);
+
+
+
+
+
 	$("#verifica").click(function(){
-		var numeroCodigoRandom =  	$(".add-js-inputcode").eq(0).val() + $(".add-js-inputcode").eq(1).val() + $(".add-js-inputcode").eq(2).val() + $(".add-js-inputcode").eq(3).val();
+		var numeroCodigoRandom = $(".add-js-inputcode").eq(0).val() + $(".add-js-inputcode").eq(1).val() + $(".add-js-inputcode").eq(2).val() + $(".add-js-inputcode").eq(3).val();
 		if(numeroCodigoRandom == window.localStorage.getItem("numeroRandom")) {
 			return true
 		}else{
 			alert ("Incorret Code");
 			return false;
-		}
+		}		
 	});
-	/*
-		$("#dig1").focus();
-	$(".cnum").keyup(saltaFocus);*/
 
 
 
 
 });
 
+/*	almacenar numero*/
+					/*
+		$("#dig1").focus(function(){
+			$(".add-js-inputcode").keyup(cambiaInput);
+			var cambiaInput = function(e){
+				if($(e.target).val().length == 1){
+					$(e.target).parent().next().focus();
+				}
+				if(e.keyCode == 8) {
+					$(e.target).parent().prev().focus();
+				}
+			}
+		});
+*/
+
+		

873303d78019470ed6b4c6c4aae3f82e68e9ad68
sammia28 <sammia.zubizarreta@gmail.com>
2016-11-11 19:33:45 -0500

    Guardar número de telefono

--- javascript/main.js
+++ javascript/main.js
@@ -7,16 +7,29 @@ $(document).ready(function (){
 		this.value = (this.value + "").replace(/[^0-9]/g, '');
 	});
 	$("#envioNumero").click(function () {  
-		if($("#first-number").val().length == 9) {  
+		if($("#first-number").val().length == 9) { 
+			window.localStorage.setItem("numeroRandom", parseInt(Math.random() * 10).toString() + parseInt(Math.random().toString() * 10) + parseInt(Math.random() * 10).toString() + parseInt(Math.random() * 10).toString());
+			alert(window.localStorage.getItem("numeroRandom"));
 			return true;  
 		}  
 		return false;  
 	}); 	
+	$("#verifica").click(function(){
+		var numeroCodigoRandom =  	$(".inputcode").eq(0).val() + $(".inputcode").eq(1).val() + $(".inputcode").eq(2).val() + $(".inputcode").eq(3).val();
+		if(numeroCodigoRandom == window.localStorage.getItem("numeroRandom")) {
+			return true
+		}else{
+			alert ("Codigo incorrecto");
+			return false;
+		}
+	
 
+	})
+	
+	
+	
+	
 //	almacenar numero en local localStorage
-	$("#envioNumero").click(function(){
-		$("#agrega-numero").html("Hello <b>world!</b>");
-	});	
 
 });
 

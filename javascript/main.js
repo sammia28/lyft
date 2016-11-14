815172b1e342815c64fcce05aa95bcf50573c573
Sammia Karen Zubizarreta Arimuya <Sammia Karen Zubizarreta Arimuya>
2016-11-14 01:12:37 -0500

    mostrar número de telefono guardado

--- javascript/main.js
+++ javascript/main.js
@@ -4,54 +4,46 @@ $(document).ready(function(){
 
 $(document).ready(function (){
 
-	var obtNumCel = $("#first-number").val();
+	var numeroIngresado = window.localStorage.getItem("numCelular");
+	var generaNunRandom = window.localStorage.getItem("numeroRandom");
+
+
 	$("#first-number").keyup(function (){
+		var numCelIngresado = $("#first-number").val();
 		this.value = (this.value + "").replace(/[^0-9]/g, '');
-		window.localStorage.setItem("numCelular", obtNumCel);
+		window.localStorage.setItem("numCelular", numCelIngresado);
 	});
+
 	$("#envioNumero").click(function() {  
-		if($("#first-number").val().length == 9) { 
-
-			var codigoRandom = parseInt(Math.random() * 10).toString() + parseInt(Math.random().toString() * 10) + parseInt(Math.random() * 10).toString() + parseInt(Math.random() * 10).toString()
-
-			window.localStorage.setItem("numeroRandom", codigoRandom );
-			alert(window.localStorage.getItem("numeroRandom"));
+		if($("#first-number").val().length == 9){ 
+			var codigoRandom = Math.floor(Math.random()*9000)+900;
+			var numeroRandom = window.localStorage.setItem("numeroRandom", codigoRandom);
+			alert(generaNunRandom);
 			return true;  
-			$("#cel").text(localStorage.getItem("numCelular")); 
 		}else{
 			alert("The number is wrong");
 			return false;
 		}
 	}); 
 
-
-
-
-
+	$("#cel").text(numeroIngresado);
+	$("#newCode").click(function(){
+		alert(generaNunRandom);
+	});
+	
 	$("#verifica").click(function(){
-		var numeroCodigoRandom = $(".add-js-inputcode").eq(0).val() + $(".add-js-inputcode").eq(1).val() + $(".add-js-inputcode").eq(2).val() + $(".add-js-inputcode").eq(3).val();
-		if(numeroCodigoRandom == window.localStorage.getItem("numeroRandom")) {
+		var numeroCodigo = $(".add-js-inputcode").eq(0).val() + $(".add-js-inputcode").eq(1).val() + $(".add-js-inputcode").eq(2).val() + $(".add-js-inputcode").eq(3).val();
+		if(numeroCodigo == generaNunRandom) {
 			return true
 		}else{
 			alert ("The code is wrong");
 			return false;
 		}		
 	});
+
 });
 
-/*	almacenar numero*/
-					/*
-		$("#dig1").focus(function(){
-			$(".add-js-inputcode").keyup(cambiaInput);
-			var cambiaInput = function(e){
-				if($(e.target).val().length == 1){
-					$(e.target).parent().next().focus();
-				}
-				if(e.keyCode == 8) {
-					$(e.target).parent().prev().focus();
-				}
-			}
-		});
-*/
+					
+
 
 		
